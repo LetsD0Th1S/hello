cc45140108baf6d6b711ace7e84e35b61c407846
HerDeeeDurr <julian.joubert91@gmail.com>
2025-10-15 13:23:39 +0200

    Setting up first views and urls.

--- lecture3/firstapp/views.py
+++ lecture3/firstapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello there!! I'm index :)")
+
+def julian(request):
+    return HttpResponse("Julian's page")
+
+def all(request, name):
+    return HttpResponse(f"{name.capitalize()}'s brand new page!")
